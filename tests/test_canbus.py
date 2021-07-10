2df66b7703f8f1f08d7407531a237665f3326e41
Nate Costello <natecostello@gmail.com>
2021-07-10 18:00:03 +0000

    refactored to remove circular dependency

--- tests/test_canbus.py
+++ tests/test_canbus.py
@@ -2,14 +2,18 @@ from context import CanBusMonitor
 from time import sleep
 import os
 from instrument_logger import InstrumentLogger
+import can
+from can.notifier import Notifier
 
 os.system("sudo rm *.csv")
 os.system("sudo /sbin/ip link set can0 up type can bitrate 250000")
 
 
-
 try:         
-    csm = CanBusMonitor('can0', 250000)
+    csm = CanBusMonitor()
+    bus = can.interface.Bus(channel='can0', bustype='socketcan', bitrate=250000)         
+    notifier = Notifier(bus, [csm])
+    
     logger = InstrumentLogger()
     logger.addinstrument(csm)
     csm.start()
@@ -18,10 +22,15 @@ try:
     logger.start()
     sleep(10)
     logger.stop()
+
+
+    print("closing can connection")
+    notifier.stop()
+    bus.shutdown()
     os.system("sudo /sbin/ip link set can0 down")
             
 except KeyboardInterrupt:
-    print("closing can connection")
-    csm.stop()
+    print("closing can connection due to keyboard interrupt")
+    notifier.stop()
+    bus.shutdown()
     os.system("sudo /sbin/ip link set can0 down")
-
