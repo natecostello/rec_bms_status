ab555edfcea0733dbd82407bf666b827ddbfbc1c
Nate Costello <natecostello@gmail.com>
2022-06-16 18:28:09 +0100

    updated version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="recq-NSC",  # Replace with your own username
-    version="0.0.1",
+    version="0.0.2",
     author="Nate Costello",
     author_email="natecostello@gmail.com",
     description="A package that collects information from the REC Q Battery Management System",
